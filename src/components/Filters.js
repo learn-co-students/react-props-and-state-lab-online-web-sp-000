6d7fa17dfdc149509934a65e45382a508cb2aaa5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-12 10:53:41 -0400

    refactor PetBrowser

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 class Filters extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,17 +3,14 @@ import React from "react";
 import Pet from "./Pet";
 
 class PetBrowser extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const petItems = this.props.pets.map(pet => {
-      return <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet}/>
-    });
-    return <div className="ui cards">
-      { petItems }
-    </div> ;
+    return (
+      <div className="ui cards">
+        {this.props.pets.map(pet => (
+          <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet} />
+        ))}
+      </div>
+    );
   }
 }
 
