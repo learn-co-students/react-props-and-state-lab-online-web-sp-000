7b34043488e6409abab352c191b978d7b7cd24a3
Rafael <RafaelB24@yahoo.com>
2019-08-13 19:01:48 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,44 @@ class App extends React.Component {
         type: 'all'
       }
     }
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onFindPetsClick = this.onFindPetsClick.bind(this);
+    this.onAdoptPet = this.onAdoptPet.bind(this);
+  }
+
+  onChangeType(newType){
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: newType.target.value
+      }
+    });
+  }
+
+  onFindPetsClick(){
+    const type = this.state.filters.type;
+    let url = '';
+    if(type === 'all'){
+      url = '/api/pets'
+    } else if(this.state.filters.type) {
+      url = `/api/pets?type=${type}`
+    }
+console.log(this.state.filters.type)
+    fetch(url)
+      .then((response) => {
+        // this.state.pets.push(response.json())
+        this.setState({
+          pets: response.json()
+        })
+      })
+      .catch(error => console.error(error));
+  }
+
+  onAdoptPet(id){
+    const pets = this.state.pets.map((pet) => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet;
+    });
+    this.setState({pets});
   }
 
   render() {
@@ -24,10 +62,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
