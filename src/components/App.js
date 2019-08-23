5e9ee8c9b87e14a1b4623da1655f26fb695f5eee
github username <github email address>
2019-08-23 08:15:13 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,40 @@ class App extends React.Component {
     }
   }
 
+    onChangeType = (event) => {
+      this.setState({
+        filters: {
+          type: event.target.value
+        }
+      })
+    }
+
+    onFindPetsClick = () => {
+      let url = "/api/pets"
+      if (this.state.filters.type !== 'all') {
+        url += `?type=${this.state.filters.type}`
+      }
+
+      fetch(url)
+      .then(r => r.json())
+      .then(petsJSONArray => {
+        this.setState({
+          pets: petsJSONArray
+        }, () => console.log(this.state.pets))
+      })
+    }
+
+
+    onAdoptPet = (id) => {
+      let petsArrayCopy = [...this.state.pets]
+      let thePet = petsArrayCopy.find(p => p.id === id)
+      thePet.isAdopted = true
+      this.setState({
+        pets: petsArrayCopy
+      })
+
+    }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +58,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+               onChangeType={this.onChangeType}
+               onFindPetsClick={this.onFindPetsClick}
+               />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
