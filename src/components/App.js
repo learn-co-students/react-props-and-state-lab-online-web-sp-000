b06d4c3dfd2a591b4cb8c5a8ae7bd017d9b414b6
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 20:40:54 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,70 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (value) => {
+    //this allows us to know what is selected for onFindPetsClick!
+    //will be called in filter
+    //needs to update App's state.filters.type
+    this.setState({
+      ...this.state.pets,
+      filters: {
+       //take note, not this.state.filters b/c when setting state you make keys and fill key w/ values
+        ...this.state.filters, //just in case
+        type: value
+      }
+    })
+
+  }
+
+  onFindPetsClick = () => {
+    //onChangeType registered what was selected so now can access that through state
+    //filters component calls on click
+    //app should fetch a list of pets using fetch
+    //fetch from /api/pets w/ optional query param
+    //more directions under app
+    const query = this.state.filters.type
+    if (query === "all") {
+      fetch(`/api/pets`)
+      .then(resp => resp.json()) //json is a function //if do single line fetch don't need brace
+      .then(json => {
+        this.setState({
+          pets: json,
+          ...this.state.filters
+        })
+      })
+    } else {
+      fetch(`/api/pets?type=${query}`)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          pets: json,
+          ...this.state.filters
+        })
+      })
+    }
+//can now access currently selected pets w/ this.state.pets
+  }
+
+  onAdoptPet = (id) => {
+    //state is all the pets so can't do that
+    //instead find pet using id and flip isAdopted
+    const changePet = this.state.pets.map((pet) => {
+      if (pet.id === id) {
+        //return whole new pet object
+        return {...pet, isAdopted: true} //not pet.isAdopted b/c creating a pet
+      } else {
+        return pet
+        //breaks out of if and goes to next iteration, will return all pets
+        //since returns all the pets, easier to set state
+      }
+    })
+    this.setState({
+      pets: changePet,
+      ...this.state.filters
+
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +88,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+                />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet}
+              />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  generatePetCards = () => {
+
+    //want to do one at a time so can pass in one pet at a time
+    return this.props.pets.map(pet => {
+      return <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet} />
+    })
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">{this.generatePetCards()}</div>
   }
 }
 
