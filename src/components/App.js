4a6871dbd9d2040d49149217d6187c598b119f5e
Alan Krajina <alankrajina88@gmail.com>
2019-10-21 15:19:13 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
+
 class App extends React.Component {
   constructor() {
     super()
@@ -15,6 +16,61 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+//        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+  //  console.log("clicked")
+    let url = '/api/pets'
+    if (this.state.filters.type !== "all"){
+      // concating to the end of url after /pets
+      url += `?type=${this.state.filters.type}`
+      // now if it is "all" it goest by default to root url
+    }
+
+    fetch(url)
+         .then(result => result.json())
+         // .then(console.log)
+         // (22) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+
+         .then(animals => {
+            this.setState({
+              pets: animals    // updejta pets state prema fetchanim podacima
+            })
+    })
+  }
+
+
+    onAdoptPet = (id) => {
+    //  console.log("clicked") - radi jer smo poslali iz APP u PetBrowser pa u Pet
+    //  console.log(thePet) -  after code below
+    //  {id: "5c142d9e-ea45-4231-8146-cccf71c704c0", type: "dog", gender: "male", age: 4, weight: 1, …}
+
+    // 1. make a copy or orig array, 2. change pet property, 3. replace array
+
+      let petsArrayCopy = [...this.state.pets]
+      const thePet = this.state.pets.find(pet => pet.id === id)
+      thePet.isAdopted = true
+      this.setState({ pets: petsArrayCopy })
+        // react dev tools -> isAdopted: true
+
+
+
+
+    //  const currentState = pet.isAdopted;
+    //  console.log(currentState)
+    //  false
+    //  this.setState({ isAdopted: !currentState }) //, () => console.log(this.state.isAdopted))
+    };
+
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,11 +80,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+            <Filters
+            onChangeType={this.onChangeType}
+            onFindPetsClick={this.onFindPetsClick}
+            />
             </div>
+
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
+
           </div>
         </div>
       </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,26 +2,45 @@ import React from 'react'
 
 class Pet extends React.Component {
   render() {
+// destructure incoming object from PetBrowser
+    const {name, type, age, weight, gender , isAdopted, id} = this.props.pet
+
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+
+            {(gender === "male") ? "♂" : "♀"}
+
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
+
+
+// if isAdopted true -> shows ONLY Already adopted Button, else ONLY shows Adopt pet button
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {isAdopted === true ? (
+            <button className="ui disabled button">Already adopted</button>
+          ) : (
+            <button
+              className="ui primary button"
+              onClick={() => this.props.onAdoptPet(id)}
+            >
+              Adopt pet
+            </button>
+          )}
         </div>
+
+
       </div>
+                    // onClick expression in order to give arguments
     )
   }
 }
