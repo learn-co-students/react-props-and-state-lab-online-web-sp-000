6be7ff711b31686afa761f5a2c4e20e848bd486f
rayvaldez <ray_valdez@hotmail.co.uk>
2019-11-06 21:54:46 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
+const API = '/api/pets'
+
 class App extends React.Component {
   constructor() {
     super()
@@ -15,6 +17,40 @@ class App extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  handleClick = () => {
+    if (this.state.filters.type === 'all') {
+      fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ pets: data.pets}))
+    } else if (this.state.filters.type === 'cat') {
+      fetch(API + '?type=cat')
+      .then(response => response.json())
+      .then(data => this.setState({ pets: data.pets}))
+    } else if (this.state.filters.type === 'dog') {
+      fetch(API + '?type=dog')
+      .then(response => response.json())
+      .then(data => this.setState({ pets: data.pets}))
+    } else if (this.state.filters.type === 'micropig') {
+      fetch(API + '?type=micropig')
+      .then(response => response.json())
+      .then(data => this.setState({ pets: data.pets}))
+    }
+  }
+
+  handleAdopt = (event) => {
+    const pets = this.state.pets.map(pet => {return pet.id === event ? {...pet, isAdopted: true} :pet})
+    this.setState({ pets })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +60,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChange} onFindPetsClick={this.handleClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdopt}/>
             </div>
           </div>
         </div>
