adda07e92923eb33938007143c67195b9e5b2be4
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-01-01 01:11:13 -0800

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,37 @@ class App extends React.Component {
     }
   }
 
+  changeType = type => {
+    this.setState({
+      filters: {...this.state.filters, type: type}
+    })
+    console.log(this.state.pets)
+  }
+
+  findPets = () => {
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets')
+        .then(response => response.json())
+        .then(pets => this.state.pets = pets)
+        .catch(error => console.log(error))
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+        .then(response => response.json())
+        .then(pets => this.state.pets = pets)
+        .catch(error => console.log(error))
+    }
+  }
+
+  adoptPet = petId => {
+    let foundPet = Object.assign({}, this.state.pets.find(pet => pet.id === petId))
+    foundPet.isAdopted = true
+    this.setState({
+      {...this.state.pets}
+    })
+  }
+
   render() {
-    return (
+    return(
       <div className="ui container">
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
@@ -24,10 +53,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeType} onFindPetsClick={this.findPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.adoptPet} />
             </div>
           </div>
         </div>
@@ -37,3 +66,21 @@ class App extends React.Component {
 }
 
 export default App
+
+// findPets = () => {
+//   fetch('/api/pets')
+//     .then(response => response.json())
+//     .then(pets => {
+//       if (this.state.filters.type === "all") {
+//         this.setState({ pets: pets})
+//       } else {
+//         let petArray = []
+//         pets.forEach(pet => {
+//           if (pet.type === this.state.filters.type) {
+//             petArray.push(pet)
+//           }
+//         })
+//         this.setState({ pets: petArray})
+//       }
+//     })
+// }
