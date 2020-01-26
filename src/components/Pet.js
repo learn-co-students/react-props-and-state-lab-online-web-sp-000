2a9de9754f5d4cd77c100e9f2a7adc91bd810b20
davisjustinw <davisjustinw@gmail.com>
2020-01-25 22:18:34 -0800

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  handleAdoptPet = (event) => {
+
+    this.props.onAdoptPet(this.props.pet.id)
+  }
+
+  showButton = () => {
+    if(this.props.pet.isAdopted){
+      return <button className="ui disabled button" >Already adopted</button>
+    } else {
+      return <button onClick={event => this.handleAdoptPet(event)} className="ui primary button" >Adopt pet</button>
+    }
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.name}{' '}
+            {
+              {
+                male: '♂',
+                female: '♀'
+              }[this.props.pet.gender]
+            }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
+
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.showButton()}
         </div>
       </div>
     )
