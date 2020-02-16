9e2901905c2c5c862bdecac8a0307d0fb13cb521
ch2isk4kos <ch2isk4kos@gmail.com>
2020-02-16 15:22:55 -0500

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
 class Filters extends React.Component {
-  render() {
+    render() {
     return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
+        <div className="ui form">
+            <h3>Random myArray Mapping</h3>
+            <div>
+                {this.props.myArray.map(num => num * num)}
+            </div>
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
+            <h3>Animal type</h3>
+            <div className="field">
+                <select
+                    onChange={this.props.onChangeType}
+                    name="type"
+                    id="type">
+
+                    <option value="all">All</option>
+                    <option value="cat">Cats</option>
+                    <option value="dog">Dogs</option>
+                    <option value="micropig">Micropigs</option>
+                </select>
+            </div>
+
+            <div className="field">
+                <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
+            </div>
+
         </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Filters
+
+// {this.props.myArray.map(num => return { num * num } )}
+// {this.props.myArray.map(function(num) {return num * num} )}
