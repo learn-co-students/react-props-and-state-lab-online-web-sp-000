df140d7e3210d38f687d29c474ea2fa15eb33482
wyao905 <warrenyca905@gmail.com>
2020-03-11 16:40:07 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,46 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isAdopted: this.props.pet.isAdopted
+    }
+  }
+
+  handleClick = (event) => {
+    this.props.onAdoptPet(this.props.pet.id)
+    this.setState({
+      isAdopted: true
+    })
+  }
+
+  adoptButton = () => {
+    if(this.state.isAdopted) {
+      return <button className="ui disabled button">Already adopted</button>
+    } else {
+      return <button className="ui primary button" onClick={(event) => this.handleClick(event)}>Adopt pet</button>
+    }
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {(this.props.pet.gender === 'male') ? '♂' : '♀'}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.adoptButton()}
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,14 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  showPets = (array) => (
+    array.map(petInfo => {
+      return <Pet pet={petInfo} onAdoptPet={this.props.onAdoptPet}/>
+    })
+  )
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">{this.showPets(this.props.pets)}</div>
   }
 }
 
