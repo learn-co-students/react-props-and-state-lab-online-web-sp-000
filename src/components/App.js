23bec585146d4370e9c0aee42c78729ce7afac45
freddysilber <freddy.silber@cloudcoach.com>
2020-04-10 21:38:44 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,44 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+  }
+
+  fetchPets = () => {
+    let endpoint = '/api/pets'
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`
+    }
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(pets => this.setState({
+        pets: pets
+      }))
+  }
+
+  onChangeType = ({ target: { value } }) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: value
+      }
+    })
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet
+    })
+    this.setState({ pets: pets })
   }
 
   render() {
@@ -24,10 +50,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
@@ -35,5 +61,3 @@ class App extends React.Component {
     )
   }
 }
-
-export default App
