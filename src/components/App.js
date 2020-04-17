130064956a18fdeb807833e761353f75037ffbc8
Blythe Pollard <blytheepollard@gmail.com>
2020-04-17 17:01:18 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = event => {
+    this.setState({
+      ...this.state.filters,
+      type: event.target.value
+    })
+  }
+
+  fetchPetsList = () => {
+    let url = '/api/pets'
+    if(this.state.filters.type === 'all') {
+       let url = '/api/pets'
+    } else if(this.state.filters.type === 'cat') {
+       url += `?type=${this.state.filters.type}`
+    } else if(this.state.filters.type === 'dog') {
+       url += `?type=${this.state.filters.type}`
+    } else if(this.state.filters.type === 'micropig') {
+       url += `?type=${this.state.filters.type}`
+    }
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({pets: pets}),
+      )
+  }
+
+  onAdoptPet = petId => {
+    const theRightPet = this.state.pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet //
+    })
+    //theRightPet.props = {isAdopted: true}
+    this.setState({pets : theRightPet})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,15 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType} //these are props, not events- getting triggered when called in child components!
+                onFindPetsClick={this.fetchPetsList} 
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+                onAdoptPet={this.onAdoptPet} //need to finish building this out
+              />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  render() { //map over pets prop, for each pet create new pet component & onAdoptPet function
+    const petCards = this.props.pets.map(pet => (
+      <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet}/>
+      ));
+    return <div 
+      className="ui cards">
+      {petCards}  
+    </div> 
   }
 }
 
 export default PetBrowser
+
+//in react and doing .map, always have to have a key!!
