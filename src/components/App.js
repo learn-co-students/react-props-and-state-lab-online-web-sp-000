df8ac6535d373dde8c60a110dc073d01ace14276
github username <github email address>
2020-08-05 22:14:36 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,54 @@ import PetBrowser from './PetBrowser'
 class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+  }
+
+  changeFilterType = (e) => {
+    this.setState({
+      filters: {
+        type: e.target.value
+      }
+    })
+  }
+
+  fetchPets = () => {
+      fetch('/api/pets')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          pets: json
+        })
+      })
+  if (this.state.filters.type !== 'all') { 
+    const url = `/api/pets?type=${this.state.filters.type}`
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          pets: json
+        })
+      })
+    }
+  }
+
+  handleAdoptPet = (id) => {
+    const adoptedPet = this.state.pets.find(pet => pet.id === id)
+    const index = this.state.pets.findIndex(pet => pet.id === id)
+    adoptedPet.isAdopted = true
+    this.setState({
+      pets: [
+        ...this.state.pets.slice(0, index),
+        adoptedPet,
+        ...this.state.pets.slice(index + 1)
+              
+      ]
+    })
   }
 
   render() {
@@ -24,10 +65,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeFilterType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
 class Filters extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      optionValue: ""
+    }
+  }
+
+  handleOptionChange = (e) => {
+    this.setState({
+      optionValue: e.target.value
+    })
+  }
+
+  onFindPetsClick = () => {
+    this.props.onChangeType(this.state.optionValue)
+  }  
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={event => this.props.onChangeType(event)}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +33,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button onClick={this.props.onFindPetsClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
