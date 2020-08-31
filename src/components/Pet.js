e30531fb9a086474974213b820296c6a6534c4f5
juan siu <juan.siu@gmail.com>
2020-08-31 00:34:40 +0000

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,26 +1,88 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      adoptedStatus: this.props.pet.isAdopted
+    }
+  }
+
+  handleClick = (event, id) => {
+    console.log('click')
+    if (this.props.pet.isAdopted === false){
+      this.props.onAdoptPet(id)
+      this.setState({
+        adoptedStatus: this.props.pet.isAdopted
+      });
+    }
+
+  }
+
+  handleGender = () => {
+    console.log('handling gender')
+    if (this.props.pet.gender === 'male'){
+      return ' ♂ '
+    } else if (this.props.pet.gender === 'female'){
+      return ' ♀ '
+    }
+  }
+
+  handleAdopted = () => {
+    console.log('handling adopted')
+    if (this.state.adoptedStatus === true){
+      console.log('adopted')
+      return <button className="ui disabled button">Already adopted</button>
+    } else if (this.state.adoptedStatus === false) {
+      console.log('not adopted')
+      return <button onClick={ (event) => this.handleClick(event, this.props.pet.id)} className="ui primary button">Adopt pet</button>
+    }
+  }
+
+  //adopted = () => {
+  //  console.log('rendering adopted')
+  //  return (
+  //  <div className="extra content">{
+  //    this.props.pet.isAdopted
+  //    ? <button className="ui primary button">Already adopted</button>
+  //    : <button className="ui disabled button">Adopt pet</button>
+  //  }
+  //  </div>
+  //  )
+  //}
+//
+  //notAdopted = () => {
+  //  console.log('rendering not adopted')
+  //  return (
+  //  <div className="extra content">{
+  //    this.props.pet.isAdopted
+  //    ? <button className="ui disabled button">Already adopted</button>
+  //    : <button onClick={ (event) => this.handleClick(event, this.props.pet.id)} className="ui primary button">Adopt pet</button>
+  //  }
+  //  </div>
+  //  )
+  //}
+
   render() {
+    console.log('rendering pet')
     return (
       <div className="card">
         <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
+          <p className="header">
+          {this.handleGender()}
+          {this.props.pet.name}
+          </p>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
-        </div>
+        {this.handleAdopted()}
       </div>
     )
   }
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  mapPet = () => {
+    console.log('mapping pets')
+    return this.props.pets.map((o, index)=> <Pet key={index} pet={o} onAdoptPet = {this.props.onAdoptPet}/>)
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    console.log('rendering pet browser')
+    return <div className="ui cards">
+      {this.mapPet()}
+    </div>
   }
 }
 
