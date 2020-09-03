537c020af4fc10e7eb9890e9fbc255bb7c0a1d1f
mbade1 <mbade314@gmail.com>
2020-09-03 15:41:04 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React from 'react';
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
 
 class App extends React.Component {
   constructor() {
-    super()
-
+    super();
     this.state = {
       pets: [],
+      adoptedPets: [],
       filters: {
-        type: 'all'
+        type: 'all',
       }
+    };
+  }
+
+  fetchPets = () => {
+    let url = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`;
     }
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }));
+
+  }
+
+  handleChangeFilterType = type => {
+    this.setState({
+      filters: Object.assign({}, this.state.filters, {
+        type: type,
+      })
+    });
+  }
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets.map((p) => {
+      if (p.id === id){
+        return {...p, isAdopted: true }
+      } else {
+        return p
+      }
+    })
+    this.setState({
+      pets: pets
+    })
   }
 
   render() {
@@ -24,16 +57,24 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                filters={this.state.filters}
+                onChangeType={this.handleChangeFilterType}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                 pets={this.state.pets}
+                 adoptedPets={this.state.adoptedPets}
+                 onAdoptPet={this.onAdoptPet}
+              />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
