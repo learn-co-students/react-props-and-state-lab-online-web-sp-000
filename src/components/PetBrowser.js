2b942a947e4641b44e62ff5e13d1181408abc296
github username <github email address>
2020-09-25 20:42:55 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 import Pet from './Pet'
 
+//pets proop
+//array of pets
+//onAdoptPet prop. This callback prop gets passed to its <Pet />
 class PetBrowser extends React.Component {
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    let petsArray = this.props.pets 
+    let petsList = petsArray.map((petObj) => {
+      return <div className="ui cards"><Pet onAdoptPet = {this.props.onAdoptPet} pet={petObj}/></div>
+    })
+    return (
+      <React.Fragment>
+        {petsList}
+      </React.Fragment>
+    )
   }
 }
 
