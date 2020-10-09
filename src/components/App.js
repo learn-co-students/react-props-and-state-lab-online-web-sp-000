568de9d4e6606ccbe9d493507375cab1dc169e9f
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-08 20:23:19 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -29,10 +29,12 @@ class App extends React.Component {
     }
     }
 
-    onAdoptPet = (id) => {
-      const pet = this.state.pets.find(pet => pet.id === id);
-      pet.isAdopted = true;
-    }
+    onAdoptPet = petId => {
+      const pets = this.state.pets.map(p => {
+        return p.id === petId ? { ...p, isAdopted: true } : p;
+      });
+      this.setState({ pets: pets });
+    };
 
   render() {
     return (
