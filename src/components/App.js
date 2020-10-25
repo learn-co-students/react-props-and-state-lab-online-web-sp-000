85936611e669801c5437d91a653813364101c016
Christopher Dent <christopherdent01@gmail.com>
2020-10-24 21:47:42 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,19 +15,54 @@ class App extends React.Component {
     }
   }
 
+
+  onChangeType = (childState) => {
+    let newState = childState
+    this.setState({
+          filters: Object.assign({}, this.state.filters, {
+          type: newState //here is where you'll assign the type, cat, dog etc.  I passed the child's state up here with a callback.
+        }, () => this.state.type)
+      }, console.log(newState))
+      // this.onFindPetsClick(newState))// this waas just for testing i dont think i need to call it here
+      //this callback needs to update state.filters.type (to cat, dog, micropig)
+  }
+
+
+
+  onFindPetsClick = () => {
+      let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }));
+  };
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  };
+
+ 
   render() {
     return (
       <div className="ui container">
         <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
+          <h1 className="ui dividing header">React Animal Shelter </h1>
         </header>
+
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,41 @@
 import React from 'react'
 
 class Filters extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { val: "" };
+  }
+
+
+  // handleChange = (event) => {
+  //   this.setState({
+  //     val: event.target.value
+  //   }, () => this.state.val)  //using the second arg for setState, an anon function, is the only way to get it to update state immediately
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      val: event.target.value
+    }, this.props.onChangeType(event.target.value))  //using the second arg for setState, an anon function, is the only way to get it to update state immediately
+  }
+
+  handleClick = (event) => {
+    console.log("HI")
+  }
+
+  callback = () => {
+    // this.props.onChangeType(this.state.val)
+    // console.log(this.state)
+    this.props.onFindPetsClick(this.state.val)
+  }
+
   render() {
     return (
       <div className="ui form">
-        <h3>Animal type</h3>
+        <h3>Animal type {this.state.val} </h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleChange}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,8 +44,9 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.callback}>Find pets</button>
         </div>
+
       </div>
     )
   }
