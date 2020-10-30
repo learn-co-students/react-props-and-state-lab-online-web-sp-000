e15871c50182b87b1d411bc7d3dfbda4ef25a801
ting682 <tchung682@gmail.com>
2020-10-30 11:00:33 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,9 +2,36 @@ import React from 'react'
 
 import Pet from './Pet'
 
+
 class PetBrowser extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pets: this.props.pets
+    }
+
+    this.adoptPet = this.adoptPet.bind(this)
+  }
+
+  adoptPet = (pet_id) => {
+   
+    
+  }
+
+  genPets = () => {
+    
+    
+     return (
+       this.props.pets.map(pet => {
+        return <Pet  pet={pet} onAdoptPet={pet.id}/>
+       })
+     )
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  return <div className="ui cards">{this.genPets()}</div>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './fetch-setup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App onChangeType={event => this.updateFilters(event)}/>, document.getElementById('root'));
