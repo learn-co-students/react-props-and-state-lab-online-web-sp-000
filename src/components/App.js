d85c5db88d01be112c4e87c766331c067e178705
Sarah Condon <s.j.condon01@gmail.com>
2021-02-08 18:32:34 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,35 @@ class App extends React.Component {
       }
     }
   }
+  onAdoptPet = (id) => {
+    let pets = [...this.state.pets]
+    let adoptedPet = pets.find(pet => pet.id === id)
+    adoptedPet.isAdopted = true
+    this.setState({ pets })
+  }
+
+  onChangeType = (type) => {
+    this.setState({
+      filters: {
+        type: type
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let query = ""
+    if(!(this.state.filters.type === "all")){
+      console.log("HERE")
+      query = `?type=${this.state.filters.type}`
+    }
+    const url = `/api/pets${query}`
+    fetch(url)
+    .then(res => res.json())
+    .then(pets => {
+      this.setState({ pets })
+    })
+    .catch(e => alert(e))
+  }
 
   render() {
     return (
@@ -24,10 +53,14 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
