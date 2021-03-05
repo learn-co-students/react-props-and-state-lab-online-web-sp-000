5c8e51013f2df2490e051be78c227e4cd44c861a
zacharytq <zacharytq@gmail.com>
2021-03-05 13:32:20 -0500

    working

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -7,14 +7,15 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.gender === 'male' ? '♂' : '♀'}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
